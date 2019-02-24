cfb56b2f705dad4355514a4ed5a856c2c9c48070
zgy朱光阳 <gy.zhu@ctrip.com>
2019-02-24 22:33:40 +0800

    BaseBusiness

--- upload.sh
+++ upload.sh
@@ -6,7 +6,7 @@ git stash pop
 VersionString=`grep -E 's.version.*=' BaseBusiness.podspec`
 VersionNumber=`tr -cd 0-9 <<<"$VersionString"`
 #NewVersionNumber=$(($VersionNumber + 2))
-NewVersionNumber=0.1.1
+NewVersionNumber=0.1.2
 LineNumber=`grep -nE 's.version.*=' BaseBusiness.podspec | cut -d : -f1`
 sed -i "" "${LineNumber}s/${VersionNumber}/${NewVersionNumber}/g" BaseBusiness.podspec
 
