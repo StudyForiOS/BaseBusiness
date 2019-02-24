8e49b312005b1cd5b483b06847dd7f907d353700
zgy朱光阳 <gy.zhu@ctrip.com>
2019-02-24 22:01:02 +0800

    BaseBusiness

---
+++ BaseBusiness/Classes/BaseBusiness.swift
@@ -0,0 +1,17 @@
+//
+//  BaseBusiness.swift
+//  BaseBusiness
+//
+//  Created by ForJade Giant on 2019/2/24.
+//
+
+import UIKit
+
+open class BaseBusiness: NSObject {
+
+    
+    open func businessJob(_ str: String,params: Array<Any>) -> Any? {
+        return nil
+    }
+    
+}
